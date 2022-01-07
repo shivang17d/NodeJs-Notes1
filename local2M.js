0bc23a24de47ccb10d1733cb744a1d00ba9fbc05
Shivang Gupta <shivangguptaxd@gmail.com>
2022-01-07 18:36:10 +0530

    Create local2M.js

---
+++ local2M.js
@@ -0,0 +1,11 @@
+// LOCAL MODULE : Type 2
+
+const loc = require('./local2')
+
+// loc.n('Shivang Gupta')
+// loc.f('Chicken Curry','Mushroom')
+
+// OR
+
+loc.name('Shivang Gupta')
+loc.food('Chicken Curry','Mushroom')
