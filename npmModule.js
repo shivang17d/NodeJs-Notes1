a8e00a47df3630dca5c9423681f61996b709800b
Shivang Gupta <shivangguptaxd@gmail.com>
2022-01-07 17:38:48 +0530

    Create npmModule.js

---
+++ npmModule.js
@@ -0,0 +1,13 @@
+//Below Code Run in the background which is not seen :
+// (function(exports,require,module,__filename,__dirname){
+//
+// })()
+    
+
+// NPM MODULE
+
+const rang = require('cli-color')
+console.log(rang.green('Shivang Gupta'))
+console.log(rang.bgRed('Lucky Gupta'))
+console.log(rang.green.bgYellow('Shiv Gupta'))
+console.log(rang.bgYellow.red('Luck Gupta'))
